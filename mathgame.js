8d1dd871d53ba125c4f8b4dcb76202cd539c1b08
Alex Rivadeneira <alexrivs@gmail.com>
2015-02-23 10:32:51 -0500

    templates

--- mathgame.js
+++ mathgame.js
@@ -1,3 +1,18 @@
+questionsList = new Mongo.Collection("questions");
+
+// clears the collection (otherwise would accumulate on every load)
+questionsList.remove({});
+// adds some questions
+questionsList.insert({question: "3+3", answer: 6});
+questionsList.insert({question: "1+2", answer: 3});
+questionsList.insert({question: "6+7", answer: 13});
+questionsList.insert({question: "0+4", answer: 4});
+questionsList.insert({question: "5+6", answer: 11});
+questionsList.insert({question: "9+6", answer: 15});
+questionsList.insert({question: "1+8", answer: 9});
+
+
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('score', 0);
@@ -9,8 +24,13 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.score.events({
+  Template.questionarea.helpers({
+    showQuestions: function () {
+      return "3+3";
+    }
   });
+
+
 }
 
 if (Meteor.isServer) {
