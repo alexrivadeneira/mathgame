661f5255339295e1fbd269539e84767432b0f001
Alex Rivadeneira <alexrivs@gmail.com>
2015-02-23 10:46:19 -0500

    questions show up

--- mathgame.js
+++ mathgame.js
@@ -1,15 +1,9 @@
 questionsList = new Mongo.Collection("questions");
 
 // clears the collection (otherwise would accumulate on every load)
-questionsList.remove({});
 // adds some questions
 questionsList.insert({question: "3+3", answer: 6});
 questionsList.insert({question: "1+2", answer: 3});
-questionsList.insert({question: "6+7", answer: 13});
-questionsList.insert({question: "0+4", answer: 4});
-questionsList.insert({question: "5+6", answer: 11});
-questionsList.insert({question: "9+6", answer: 15});
-questionsList.insert({question: "1+8", answer: 9});
 
 
 
@@ -24,9 +18,9 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.questionarea.helpers({
+  Template.questionArea.helpers({
     showQuestions: function () {
-      return "3+3";
+      return questionsList.find({});
     }
   });
 
