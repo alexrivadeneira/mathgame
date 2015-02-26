331deff1f34448356a57d6ac04b10b875dfd25a8
Alex Rivadeneira <alexrivs@gmail.com>
2015-02-26 12:42:52 -0500

    please jesus work

--- allGames.js
+++ allGames.js
@@ -33,12 +33,52 @@ GameFactory.createGame = function(playerIds){
 
 
 function createPlayers(ids){
-	var players = {};
+	var players = [];
+
+	var playerOne = {};
+	var playerTwo = {};
+
+	var playerOneStats = [];
+	var playerTwoStats = [];
+
+	var playerOneContainer = {};
+	var playerTwoContainer = {};
+
+	var i = 0;
+
+	ids.forEach(function(id){
+		if (i == 0){
+			playerOneStats.push(playerOne["id"] = {id: id});
+			playerOneStats.push(playerOne["score"] = {score: 0});
+			playerOneStats.push(playerOne["username"] = {username: Meteor.users.findOne({_id: id}).username});
+
+			playerOneContainer["playerOne"] = playerOneStats;
+			players.push(playerOneContainer);
+			i++;
+		} else if (i == 1){
+			playerTwoStats.push(playerTwo["id"] = {id: id});
+			playerTwoStats.push(playerTwo["score"] = {score: 0});
+			playerTwoStats.push(playerTwo["username"] = {username: Meteor.users.findOne({_id: id}).username});
+
+			playerTwoContainer["playerTwo"] = playerTwoStats;
+			players.push(playerTwoContainer);
+			i++;
+		}
+	});
+
+/*
+	players.push(playerOne)
 
 	var playerNames = ["playerOne", "playerTwo"];
 
 	var i = 0;
 
+	playerNames.forEach(function (playerName){
+		players.push({name: })
+		i++;
+	});
+*/
+/*
 	ids.forEach(function (id){
 		players[playerNames[i]] = {
 			id: id,
@@ -47,6 +87,7 @@ function createPlayers(ids){
 		};
 		i ++;
 	});
+*/
 
 	return players;
 
